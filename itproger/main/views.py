01fb3fc3df81cf832793b561e1bd4442554dafda
Ksenia12222 <167125703+Ksenia12222@users.noreply.github.com>
2024-04-15 21:16:18 +0300

    Add files via upload

---
+++ itproger/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    data = {
+
+        }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def aboout(request):
+    return render(request, 'main/aboout.html')
